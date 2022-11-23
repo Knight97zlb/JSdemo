c1efaea19d47cb42f69288d5aabfa917407a23f3
zlb <knight97zlb@gmail.com>
2022-11-23 14:56:40 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Hello World
+// @namespace    http://k1475369.top//
+// @version      0.1
+// @description  New Script!
+// @author       Kuroichi7
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(
+    function() {
+    'use strict';
+
+    // Your code here...
+    console.log("Hello World");
+})();
